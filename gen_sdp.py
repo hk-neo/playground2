51e678fdc310a60e8471c80b978f4bab19891a1a
hk.sim <hk.sim@neobiotech.com>
2026-05-06 18:47:16 +0900

    Delete gen_sdp.py

--- gen_sdp.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-"""Generate SW Development Plan document for PLAYG-1965."""
-import pathlib
-
-out = pathlib.Path("docs/PLAYG-1965/SW_Development_Plan.md")
-out.parent.mkdir(parents=True
